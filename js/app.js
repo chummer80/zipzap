9b787943f672e44daa57e68f8e9a4d06585d8fba
chummer80 <mike.c.chu@gmail.com>
2014-05-05 03:10:33 -0700

    search ziptastic for user inputted zip
    show ziptastic results in the results panel

--- js/app.js
+++ js/app.js
@@ -1,4 +1,5 @@
 $(document).ready(function() {
+	var DEBUG = true;
 	var ENTER_KEY_CODE = 13;
 	var ZIPTASTIC_API_URL = 'http://ZiptasticAPI.com/';
 
@@ -7,6 +8,12 @@ $(document).ready(function() {
 	/***************
 	* Functions
 	****************/
+	
+	var debug = function debug(message) {
+		if (DEBUG) {
+			console.log(message);
+		}
+	};
 	
 	var isInputValid = function isInputValid() {
 		var inputString = $('#zip_input_text').val();
@@ -20,6 +27,45 @@ $(document).ready(function() {
 		});
 	}
 
+	var startAJAX = function startAJAX(zip) {
+		// Ziptastic http request
+		var xhr = $.ajax(ZIPTASTIC_API_URL + zip, {dataType: 'jsonp'})
+			.done(function(data) {
+				debug("DONE");
+				debug(data);
+				
+				// change the heading of the results panel
+				$('#zipcode').text(zip);
+				
+				if (data.error) {
+					$('#ziptastic_results')
+						.empty()
+						.html('<p>' + data.error + '</p>');
+				}
+				else {
+					data.city = toTitleCase(data.city);
+					var locationString = toTitleCase(data.city) + ", " + data.state + ", " + data.country;
+					debug("location is " + locationString);
+					
+					// clear previous Ziptastic results, then put the new results there instead
+					$('#ziptastic_results')
+						.empty()
+						.html('<p>' + locationString + '</p>');
+				}
+				
+				showResults();
+			})
+			.fail(function(data) {
+				debug("FAIL");
+			});
+	};
+	
+	var showResults = function showResults() {
+		// $('#loading').hide('blind', {direction: 'down'});
+		$('#loading').hide();
+		$('#results').fadeIn();
+	};
+	
 	/***************
 	* Event Handlers
 	****************/
@@ -27,10 +73,10 @@ $(document).ready(function() {
 	$('#zip_input_text').keypress(function(event) {
 		
 		var isDigit = function isDigit (keyCode) {
-			console.log("key entered (code): " + keyCode);
+			debug("key entered (code): " + keyCode);
 			
 			var charEntered = String.fromCharCode(keyCode);
-			console.log("key entered (char): " + charEntered);
+			debug("key entered (char): " + charEntered);
 			
 			var digitPattern = /^\d+$/;
 			return digitPattern.test(charEntered);
@@ -41,72 +87,36 @@ $(document).ready(function() {
 		}
 		else {
 			if (isDigit(event.which)) {
-				console.log("is digit");
+				debug("is digit");
 			}
 			else {
-				console.log("is not digit");				
+				debug("is not digit");				
 				event.preventDefault();
 			}
 		}
 	});
 	
 	$('#zip_input_text').keyup(function() {
-		console.log("textbox says: " + this.value);
+		debug("textbox says: " + this.value);
 		$('#zip_input_button').attr('disabled', isInputValid() ? false : true);
 	});
 		
 	$('#zip_input_button').click(function() {
-		console.log("ZAP button clicked");
+		debug("ZAP button clicked");
 		
 		var inputString = this.value;
 		if (isInputValid()) {
 			zipCode = $('#zip_input_text').val();
 			$('#input_div').slideUp();	
 			$('#loading').show('blind');
+			startAJAX(zipCode);
 		}
 	});
-		
 	
 	
 	/***************
 	* Start
 	****************/
 	
-	// Automatically show the loading gif when an ajax request is in progress.
-	$.ajaxSetup({
-		beforeSend: function() {
-			// $('#loading').show();
-		},
-		complete: function(){
-			// $('#loading').hide();
-		}
-	});
-	
 	$('#input_div').slideDown();
-	
-	// ZIPTASTIC TEST CODE
- 	zipCode = '91356';
-	var xhr = $.ajax(ZIPTASTIC_API_URL + zipCode, {dataType: 'jsonp'})
-		.done(function(data) {
-			console.log("DONE");
-			console.log(data);
-			
-			data.city = toTitleCase(data.city);
-			var locationString = toTitleCase(data.city) + ", " + data.state + ", " + data.country;
-			console.log("location is " + locationString);
-			
-			// change the heading of the results panel
-			$('#zipcode').text(zipCode);
-			
-			// clear previous Ziptastic results, then put the new results there instead
-			$('#ziptastic_results')
-				.empty()
-				.html('<p>' + locationString + '</p>');
-			
-			$('#results').fadeIn();
-		})
-		.fail(function(data) {
-			console.log("FAIL");
-		});
-		 
 });
